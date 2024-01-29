1ba1bbe00ec95cdc6be3ad1aa8d37c556af61add
Dixie Dean <d.w.sokolov26@gmail.com>
2024-01-29 18:27:38 +0300

    feat: add snippet service V1.0

--- src/main/java/com/dixie/pastebin/service/SnippetService.java
+++ src/main/java/com/dixie/pastebin/service/SnippetService.java
@@ -1,5 +1,15 @@
 package com.dixie.pastebin.service;
 
-public class PastebinService {
-    
+import org.springframework.http.ResponseEntity;
+
+public interface SnippetService {
+
+    ResponseEntity<String> create(String body, long expiration);
+
+    ResponseEntity<String> viewAll();
+
+    ResponseEntity<String> update(long id, String body);
+
+    ResponseEntity<String> delete(long id);
+
 }
--- src/main/java/com/dixie/pastebin/service/SnippetServiceImpl.java
+++ src/main/java/com/dixie/pastebin/service/SnippetServiceImpl.java
@@ -1,2 +1,29 @@
-package com.dixie.pastebin.service;public class SnippetServiceImpl {
+package com.dixie.pastebin.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SnippetServiceImpl implements SnippetService {
+
+    @Override
+    public ResponseEntity<String> create(String body, long expiration) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> viewAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> update(long id, String body) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> delete(long id) {
+        return null;
+    }
+
 }
