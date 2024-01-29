85083a5398e53ac83d80c61bba3e33fa61616019
Dixie Dean <d.w.sokolov26@gmail.com>
2024-01-29 17:31:50 +0300

    Initial Commit

---
+++ src/main/java/com/dixie/pastebin/PastebinApplication.java
@@ -0,0 +1,13 @@
+package com.dixie.pastebin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dixie/pastebin/PastebinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dixie.pastebin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
