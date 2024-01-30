05ac49ee4c27c78d79683c0629505e475ea9ddb7
Dixie Dean <d.w.sokolov26@gmail.com>
2024-01-30 14:59:02 +0300

    feat: add repository implementation V1.0

--- src/main/java/com/dixie/pastebin/repository/SnippetRepository.java
+++ src/main/java/com/dixie/pastebin/repository/SnippetRepository.java
@@ -2,11 +2,27 @@ package com.dixie.pastebin.repository;
 
 import com.dixie.pastebin.entity.Snippet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SnippetRepository extends JpaRepository<Snippet, Long> {
 
+    @Query(value = "insert into pastebin.snippets(author, body, link) " +
+            "values (:author, :body, :link) returning 'Snippet created!'", nativeQuery = true)
+    String create(@Param("author") String author, @Param("body") String body, @Param("link") String link);
 
+    @Query(value = "select * from pastebin.snippets", nativeQuery = true)
+    List<Snippet> getAllSnippets();
+
+    @Query(value = "update pastebin.snippets set body = :body " +
+            "where id = :id returning 'Snippet updated!'", nativeQuery = true)
+    String update(@Param("id") long id, @Param("body") String body);
+
+    @Query(value = "delete from pastebin.snippets where id = :id returning 'Snipped deleted!'", nativeQuery = true)
+    String delete(@Param("id") long id);
 
 }
