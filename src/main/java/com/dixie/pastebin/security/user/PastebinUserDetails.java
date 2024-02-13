5bcafcb102d91049ac576f0a7413edb410e0d304
Dixie Dean <d.w.sokolov26@gmail.com>
2024-02-13 11:12:59 +0300

    fix: make user details data not expired, not locked, etc.

--- src/main/java/com/dixie/pastebin/security/user/PastebinUserDetails.java
+++ src/main/java/com/dixie/pastebin/security/user/PastebinUserDetails.java
@@ -28,21 +28,21 @@ public class PastebinUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
