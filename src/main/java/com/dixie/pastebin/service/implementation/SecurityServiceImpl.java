5b6f8f38ceded405d2adb1aa639a0b0857e32604
Dixie Dean <d.w.sokolov26@gmail.com>
2024-02-26 13:57:36 +0300

    polishing

--- src/main/java/com/dixie/pastebin/service/implementation/SecurityServiceImpl.java
+++ src/main/java/com/dixie/pastebin/service/implementation/SecurityServiceImpl.java
@@ -17,7 +17,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
