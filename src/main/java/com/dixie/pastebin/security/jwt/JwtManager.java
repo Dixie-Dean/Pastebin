84a8de798b2f6a1cde1d7f700d10f020b05224fd
Dixie Dean <d.w.sokolov26@gmail.com>
2024-02-26 12:33:45 +0300

    chore: move secret key to properties

--- src/main/java/com/dixie/pastebin/security/jwt/JwtManager.java
+++ src/main/java/com/dixie/pastebin/security/jwt/JwtManager.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,14 @@ import java.util.Date;
 
 @Component
 public class JwtManager {
-    private static final long EXPIRATION = 1000 * 60 * 24;
-    private static final String SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    private static final long EXPIRATION_TIME = 1000 * 60 * 24;
+
+    @Value("${secret.key}")
+    private String secretKey;
 
     public String generateToken(Authentication authentication) {
         var currentDate = new Date();
-        var expirationDate = new Date(currentDate.getTime() + EXPIRATION);
+        var expirationDate = new Date(currentDate.getTime() + EXPIRATION_TIME);
 
         return Jwts.builder()
                 .setSubject(authentication.getName())
@@ -47,7 +50,7 @@ public class JwtManager {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
